c8d49757331c616c62cbfbbfea998549c08c2ff1
shivanshu1808 <108691431+shivanshu1808@users.noreply.github.com>
2025-11-22 19:08:31 +0530

    Delete building-control directory

--- building-control/src/main/java/com/building/control/repository/ApartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.building.control.repository;
-
-import com.building.control.model.Apartment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
-}
--- building-control/src/main/java/com/building/control/repository/CommonRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.building.control.repository;
-
-import com.building.control.model.CommonRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommonRoomRepository extends JpaRepository<CommonRoom, Long> {
-}
